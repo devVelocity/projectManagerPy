6c7688fcf93af86ca3a11bf3f85f994c14a6c628
VLCTY <88147999+devVelocity@users.noreply.github.com>
2022-09-20 23:20:08 +0100

    can open vscode and websites now

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from tkinter import *
 from tkinter import ttk
+import os
+import subprocess
+import webbrowser
+import sys
+
 
 root = Tk()
 frm = ttk.Frame(root, padding=10)
@@ -10,14 +15,26 @@ data = json.load(open('apps.json'))
 
 
 def btnFunction(num):
-    print(num)
+    # print(num)
+    # print(data[num])
+    for count in data[num].get("vscodepaths"):
+        path = count
+        os.system("code . /K cd "+path)
+    
+    for website in data[num].get("websites"):
+        webbrowser.open(website,new=1, autoraise=True)
+
+
+L = Label(root,text="Project Link Manager")
+
+L.pack()
 
 count = 0
 for i in data:
     print(i)
     button = ttk.Button(frm, text=f"Open {i['name']}",command=lambda :btnFunction(count))
     button.pack()
-    count = count + 1
+    count + 1
 
 frm.pack()
 root.mainloop()
